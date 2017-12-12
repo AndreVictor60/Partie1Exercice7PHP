7f199181757c53c50499cfd1a909d8730efe0fb7
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 09:56:50 +0100

    fini exercice7

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  $nom = 'Andre';
+  $prenom = 'Victor';
+  $age = 22;
+  echo 'Bonjour '.$nom.' '.$prenom.', tu as '.$age.' ans.';
+ ?>
+ <!-- Créer trois variables nom , prenom et age et les initialiser avec les valeurs de votre choix. Attention age est de type entier.
+ Afficher : "Bonjour" + nom + prenom + ",tu as" + age + "ans". -->
